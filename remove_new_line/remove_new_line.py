33db3f5ef5f931b7136d9dbd07f29b8152a7ca0d
RedPanda1618 <shumpei116@gmail.com>
2023-02-11 00:16:55 +0900

    change file_path

--- remove_new_line/remove_new_line.py
+++ remove_new_line/remove_new_line.py
@@ -1,9 +1,13 @@
+FILE_PATH = "tools/remove_new_line/text_in.txt"
+
 def main():
-    with open("tools/text_in.txt", "r") as f:
+    print("Runing... ", end="")
+    with open(FILE_PATH, "r") as f:
         txt = f.read()
     txt = txt.replace("\n", " ").replace("", "")
-    with open("tools/text_in.txt", "w") as f:
+    with open(FILE_PATH, "w") as f:
         f.write(txt)
+    print("Done.")
     
 if "__main__" == __name__:
     main()
